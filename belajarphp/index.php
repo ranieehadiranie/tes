31465099dfcd2b90e1fc9fdb86300fb762df3eff
ranieehadiranie <hadiraniebsc@gmail.com>
2024-05-26 20:30:03 +0700

    Add files via upload

---
+++ belajarphp/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>belajar php</h1>
+    <?php
+    $nama="hadiranie";
+    $ttl="yogyakarta 8 september 2006";
+    $alamat="jln sukamenak";
+    $hoby="membaca";
+    echo "nama saya : $nama <br> 
+    ttl saya : $ttl <br>
+    alamat : $alamat <br>
+    hoby : $hoby "
+    ?>
+</body>
+</html>
